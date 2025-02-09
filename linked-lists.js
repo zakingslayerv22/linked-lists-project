f9591c875ce65eb273455d31c24724c1a1e51d36
Eriwe <11926638+zakingslayerv22@users.noreply.github.com>
2025-02-09 08:47:48 -0800

    Implement append()
    In the LinkedList class, implement
    append(), a method which takes in
    a `value` and adds a new node
    containing `value` to the
    end of the list.
    
    The method traverses the list from
    the head and adds the new node to
    the end of the list.
    
    In main.js, append four lists
    and log the list to the console.

--- linked-lists.js
+++ linked-lists.js
@@ -2,6 +2,21 @@ export class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  append(value) {
+    const newNode = new Node();
+    newNode.value = value;
+    if (!this.head) {
+      this.head = newNode;
+      newNode.value = value;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.nextNode) {
+        currentNode = currentNode.nextNode;
+      }
+      currentNode.nextNode = newNode;
+    }
+  }
 }
 
 export class Node {
--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 import { LinkedList } from "./linked-lists.js";
 
 const list = new LinkedList();
+
+list.append("First Node");
+
+list.append("Second Node");
+
+list.append("Third Node");
+
+list.append("Fourth Node");
+
+console.log(list);
